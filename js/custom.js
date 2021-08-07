3b0c30980fa4b7d7a9cfc5182ad8a1222dddf74e
sstutorial <staff.sstutorial@gmail.com>
2021-08-07 18:00:57 +0530

    added banner

--- js/custom.js
+++ js/custom.js
@@ -161,6 +161,57 @@ $(document).ready(function()
 				});
 			}
 		}	
+		if($('.hero_slider-vfx').length)
+		{
+			var owl = $('.hero_slider-vfx');
+
+			owl.owlCarousel(
+			{
+				items:2,
+				loop:true,
+				smartSpeed:600,
+				autoplay:true,
+				nav:false,
+				dots:false
+			});
+
+			// add animate.css class(es) to the elements to be animated
+			function setAnimation ( _elem, _InOut )
+			{
+				// Store all animationend event name in a string.
+				// cf animate.css documentation
+				var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+				_elem.each ( function ()
+				{
+					var $elem = $(this);
+					var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
+
+					$elem.addClass($animationType).one(animationEndEvent, function ()
+					{
+						$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+					});
+				});
+			}
+
+			// Fired before current slide change
+			owl.on('change.owl.carousel', function(event)
+			{
+				var $currentItem = $('.owl-item', owl).eq(event.item.index);
+				var $elemsToanim = $currentItem.find("[data-animation-out]");
+				setAnimation ($elemsToanim, 'out');
+			});
+
+			// Fired after current slide has been changed
+			owl.on('changed.owl.carousel', function(event)
+			{
+				var $currentItem = $('.owl-item', owl).eq(event.item.index);
+				var $elemsToanim = $currentItem.find("[data-animation-in]");
+				setAnimation ($elemsToanim, 'in');
+			})
+		}
+
+
 	}
 
 	/* 
